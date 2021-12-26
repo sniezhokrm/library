bcca2c53fdca01b88539531cf69124a80905106d
sniezhokrm <sniezhokrm@gmail.com>
2021-12-26 12:57:54 +0200

    add create modal in js

--- dist/script.js
+++ dist/script.js
@@ -100,7 +100,6 @@ __webpack_require__.r(__webpack_exports__);
 
 _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.dropdown = function () {
   for (let i = 0; i < this.length; i++) {
-    console.log(this[i].getAttribute('id'));
     const id = this[i].getAttribute('id');
     Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(() => {
       console.log(2);
@@ -125,32 +124,189 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
 
 
-_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
+function calcScroll() {
+  let div = document.createElement('div');
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+  return scrollWidth;
+}
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function (created) {
+  const scroll = calcScroll();
+
   for (let i = 0; i < this.length; i++) {
     const target = this[i].getAttribute('data-target');
     Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(e => {
+      for (var i = 0; i < Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').length; i++) {
+        Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal')[i].style.left = `0px`;
+      }
+
       e.preventDefault();
       Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeIn(500);
       document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
+    });
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
+    closeElements.forEach(elem => {
+      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(elem).click(() => {
+        for (var i = 0; i < Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').length; i++) {
+          Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal')[i].style.left = `${scroll / 2}px`;
+        }
+
+        Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeOut(500);
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      });
+    });
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).click(e => {
+      if (e.target.classList.contains('modal')) {
+        for (var i = 0; i < Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').length; i++) {
+          Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal')[i].style.left = `${scroll / 2}px`;
+        }
+
+        Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeOut(500);
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      }
+    });
+  }
+};
+
+Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-toggle="modal"]').modal();
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.createModal = function () {
+  let {
+    text,
+    btns
+  } = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  const scroll = calcScroll();
+  console.log('1');
+
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id', this[i].getAttribute('data-target').slice(1)); // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+
+    const buttons = [];
+
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement('button');
+      btn.classList.add('btn', ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+
+      if (btns.settings[j][2]) {
+        btn.setAttribute('data-close', 'true');
+      }
+
+      if (btns.settings[j][3] && typeof btns.settings[j][3] === 'function') {
+        btn.addEventListener('click', btns.settings[j][3]);
+      }
+
+      buttons.push(btn);
+    }
+
+    modal.innerHTML = `
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <button class="close" data-close>
+                    <span>&times;</span>
+                </button>
+                <div class="modal-header">
+                    <div class="modal-title">
+                        ${text.title}
+                    </div>
+                </div>
+                <div class="modal-body">
+                    ${text.body}
+                </div>
+                <div class="modal-footer">
+
+                </div>
+            </div>
+        </div>
+        `;
+    modal.querySelector(".modal-footer").append(...buttons);
+    document.body.appendChild(modal);
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).modal(true);
+    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i].getAttribute('data-target')).fadeIn(500);
+  }
+};
+/*import $ from '../core';
+
+const scroll = calcScroll();
+
+
+
+$.prototype.modal = function() {
+  for (let i = 0; i < this.length; i++) {
+
+    const target = this[i].getAttribute('data-target');
+    $(this[i]).click((e) => {
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `0px`;
+      }
+      $(target).fadeIn(500);
+      document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
     });
   }
 
   const closeElements = document.querySelectorAll('[data-close]');
   closeElements.forEach(elem => {
-    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(elem).click(() => {
-      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
+    $(elem).click(() => {
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `${scroll/2}px`;
+      }
+      $('.modal').fadeOut(500);
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+
     });
   });
-  Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').click(e => {
+
+
+
+  $('.modal').click(e => {
     if (e.target.classList.contains('modal')) {
-      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `${scroll/2}px`;
+      }
+      $('.modal').fadeOut(500);
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
     }
   });
 };
 
-Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-toggle="modal"]').modal();
+function calcScroll() {
+  let div = document.createElement('div');
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
+
+$('[data-toggle="modal"]').modal();
+*/
 
 /***/ }),
 
@@ -617,6 +773,20 @@ Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-count="second"]'
 Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('button').eq(2).on('click', () => {
   Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('.w-500').fadeToggle(800);
 });
+Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('#trigger').click(() => Object(_lib_lib__WEBPACK_IMPORTED_MODULE_0__["default"])('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+  },
+  btns: {
+    count: 3,
+    settings: [['Close', ['btn-danger', 'mr-10'], true], ['Save changes', ['btn-success'], false, () => {
+      alert('Данные сохранены');
+    }], ['Another btn', ['btn-warning', 'ml-10'], false, () => {
+      alert('Hello World');
+    }]]
+  }
+}));
 
 /***/ })
 
--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,29 +1,181 @@
 import $ from '../core';
 
-$.prototype.modal = function() {
+function calcScroll() {
+  let div = document.createElement('div');
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
+
+$.prototype.modal = function(created) {
+  const scroll = calcScroll();
     for (let i = 0; i < this.length; i++) {
         const target = this[i].getAttribute('data-target');
         $(this[i]).click((e) => {
+          for (var i = 0; i < $('.modal').length; i++) {
+            $('.modal')[i].style.left = `0px`;
+          }
             e.preventDefault();
             $(target).fadeIn(500);
             document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
+        });
+
+        const closeElements = document.querySelectorAll(`${target} [data-close]`);
+        closeElements.forEach(elem => {
+            $(elem).click(() => {
+              for (var i = 0; i < $('.modal').length; i++) {
+                $('.modal')[i].style.left = `${scroll/2}px`;
+              }
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            });
+        });
+
+        $(target).click(e => {
+            if (e.target.classList.contains('modal')) {
+              for (var i = 0; i < $('.modal').length; i++) {
+                $('.modal')[i].style.left = `${scroll/2}px`;
+              }
+                $(target).fadeOut(500);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
+                if (created) {
+                    document.querySelector(target).remove();
+                }
+            }
         });
     }
-
-    const closeElements = document.querySelectorAll('[data-close]');
-    closeElements.forEach(elem => {
-        $(elem).click(() => {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
-        });
-    });
-
-    $('.modal').click(e => {
-        if (e.target.classList.contains('modal')) {
-            $('.modal').fadeOut(500);
-            document.body.style.overflow = '';
-        }
-    });
 };
 
 $('[data-toggle="modal"]').modal();
+
+$.prototype.createModal = function({text, btns} = {}) {
+
+  const scroll = calcScroll();
+  console.log('1');
+
+    for (let i = 0; i< this.length; i++) {
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+
+        // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+        const buttons = [];
+        for (let j = 0;j < btns.count; j++){
+            let btn = document.createElement('button');
+            btn.classList.add('btn', ...btns.settings[j][1]);
+            btn.textContent = btns.settings[j][0];
+            if (btns.settings[j][2]) {
+                btn.setAttribute('data-close', 'true');
+            }
+            if (btns.settings[j][3] && typeof(btns.settings[j][3]) === 'function') {
+                btn.addEventListener('click', btns.settings[j][3]);
+            }
+
+            buttons.push(btn);
+        }
+
+        modal.innerHTML = `
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <button class="close" data-close>
+                    <span>&times;</span>
+                </button>
+                <div class="modal-header">
+                    <div class="modal-title">
+                        ${text.title}
+                    </div>
+                </div>
+                <div class="modal-body">
+                    ${text.body}
+                </div>
+                <div class="modal-footer">
+
+                </div>
+            </div>
+        </div>
+        `;
+
+        modal.querySelector(".modal-footer").append(...buttons);
+        document.body.appendChild(modal);
+        $(this[i]).modal(true);
+        $(this[i].getAttribute('data-target')).fadeIn(500);
+    }
+
+};
+
+/*import $ from '../core';
+
+const scroll = calcScroll();
+
+
+
+$.prototype.modal = function() {
+  for (let i = 0; i < this.length; i++) {
+
+    const target = this[i].getAttribute('data-target');
+    $(this[i]).click((e) => {
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `0px`;
+      }
+      $(target).fadeIn(500);
+      document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
+    });
+  }
+
+  const closeElements = document.querySelectorAll('[data-close]');
+  closeElements.forEach(elem => {
+    $(elem).click(() => {
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `${scroll/2}px`;
+      }
+      $('.modal').fadeOut(500);
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+
+    });
+  });
+
+
+
+  $('.modal').click(e => {
+    if (e.target.classList.contains('modal')) {
+      for (var i = 0; i < $('.modal').length; i++) {
+        $('.modal')[i].style.left = `${scroll/2}px`;
+      }
+      $('.modal').fadeOut(500);
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+    }
+  });
+};
+
+function calcScroll() {
+  let div = document.createElement('div');
+  div.style.width = '50px';
+  div.style.height = '50px';
+  div.style.overflowY = 'scroll';
+  div.style.visibility = 'hidden';
+
+  document.body.appendChild(div);
+  let scrollWidth = div.offsetWidth - div.clientWidth;
+  div.remove();
+
+  return scrollWidth;
+}
+
+$('[data-toggle="modal"]').modal();
+*/
